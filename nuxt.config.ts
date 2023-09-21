7f04f3d9267deb1dcb7705f05fe7c225d3dd134d
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2023-09-21 18:55:13 +0330

    base: add bootstrap,sass and sass-loader package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+
+  css: ["~/node_modules/bootstrap/scss/bootstrap.scss"],
+
+  app: {
+    head: {
+      title: "trigate-hiring-tasks",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+});
